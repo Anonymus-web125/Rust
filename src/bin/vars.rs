ac3c769cb298ade7d16151862792837827bd99e2
Anonymus-web125 <bikashkumargiri23@gmail.com>
2025-06-15 06:56:48 +0530

    Added a vars file

---
+++ src/bin/vars.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let x: i32 = 5;
+    let y: i32 = 7;
+    // There are many types of integer data types in rust such as i8,i16,i32,i64 etc where 8,16,32,64 represents the number of bits
+    if x == y{
+        println!("Equals");
+    }else{
+        println!("Not equals");
+    }
+}
